bf9094cd16696086e11a8a09f8dfa71677fc4a54
Carlos Quinones <cquinones.private@gmail.com>
2026-01-21 16:44:11 -0500

    a more lively mando

--- salamander/player.gd
+++ salamander/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 
 
+@onready var visual: Node3D = %SalamanderVisual
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
@@ -10,19 +12,13 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = 0
+		velocity.z = 0
 
 	move_and_slide()
